24172a20d6086424889446a7772d1616e15bdac6
Bruno Felix de Sousa <brunofelixdesousa@hotmail.com>
2021-12-07 22:56:14 -0300

    Adicionar arquivos de projeto.

---
+++ main.cpp
@@ -0,0 +1,12 @@
+﻿// CalculadoraOOP.cpp: define o ponto de entrada para o aplicativo.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
